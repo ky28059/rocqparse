1cfb75ecefa81ab3ecae751cadd14c9171090264
Kevin Yu <kevinyu567@gmail.com>
2026-02-26 20:51:56 -0500

    Use `spf`

--- lib/layout.ml
+++ lib/layout.ml
@@ -8,6 +8,8 @@ open Libnames
 let layout_vernac v =
   Pp.string_of_ppcmds @@ Ppvernac.pr_vernac v
 
+let spf = Printf.sprintf
+
 let layout_lident (l : lident) =
   "\"" ^ Id.to_string l.v ^ "\""
 
@@ -17,6 +19,11 @@ let layout_qualid (q : qualid) =
 let layout_ident_decl (lid, _l) =
   "(" ^ layout_lident lid ^ ", ...)"
 
+let layout_name (n : Name.t) =
+  match n with
+  | Name s -> spf "(Name \"%s\")" @@ Id.to_string s
+  | Anonymous -> "Anonymous"
+
 let layout_prim_token p =
   match p with
   | Number n -> NumTok.Signed.sprint n
@@ -25,24 +32,28 @@ let layout_prim_token p =
 let layout_notation_entry e =
   match e with
   | InConstrEntry -> "InConstrEntry"
-  | InCustomEntry s -> "(InCustomEntry \"" ^ s ^ "\")"
+  | InCustomEntry s -> spf "(InCustomEntry \"%s\")" s
 
 let layout_notation (e, n) =
-  "(" ^ layout_notation_entry e ^ ", \"" ^ n ^ "\")"
+  spf "(%s, \"%s\")" (layout_notation_entry e) n
 
 let rec layout_constr_expr (c : constr_expr) =
   let layout_constr_notation_subst (s, _, _, _) =
     let p = String.concat ", " @@ List.map layout_constr_expr s in
     "([" ^ p ^ "], ..., ..., ...)" in
   match c.v with
-  | CRef (q, _) -> "(CRef (" ^ layout_qualid q ^ ", ...))"
+  | CRef (id, _) -> spf "(CRef (%s, ...))" @@ layout_qualid id
   | CFix _ -> "(CFix ...)"
   | CCoFix _ -> "(CCoFix ...)"
-  | CProdN _ -> "(CProdN ...)"
+  | CProdN (_b, e) ->
+    let s = "..." in (* TODO *)
+    spf "(CProdN ([%s], %s))" s @@ layout_constr_expr e
   | CLambdaN _ -> "(CLambdaN ...)"
   | CLetIn _ -> "(CLetIn ...)"
   | CAppExpl _ -> "(CAppExpl ...)"
-  | CApp _ -> "(CApp ...)"
+  | CApp (f, args) ->
+    let s = String.concat ", " @@ List.map (fun (c, _) -> "(" ^ layout_constr_expr c ^ ", ...)") args in
+    spf "(CApp (%s, [%s]))" (layout_constr_expr f) s
   | CProj _ -> "(CProj ...)"
   | CRecord _ -> "(CRecord ...)"
   | CCases _ -> "(CCases ...)"
@@ -55,14 +66,14 @@ let rec layout_constr_expr (c : constr_expr) =
   | CEvar _ -> "(CEvar ...)"
   | CSort _ -> "(CSort ...)"
   | CCast _ -> "(CCast ...)"
-  | CNotation (_, n, s) -> "(CNotation (..., " ^ layout_notation n ^ ", " ^ layout_constr_notation_subst s ^ "))"
+  | CNotation (_, n, s) -> spf "(CNotation (..., %s, %s))" (layout_notation n) (layout_constr_notation_subst s)
   | CGeneralization _ -> "(CGeneralization ...)"
-  | CPrim p -> "(CPrim " ^ layout_prim_token p ^ ")"
+  | CPrim p -> spf "(CPrim %s)" @@ layout_prim_token p
   | CDelimiters _ -> "(CDelimiters ...)"
   | CArray _ -> "(CArray ...)"
 
 let layout_proof_expr (id, (_, expr)) =
-  "(" ^ layout_ident_decl id ^ ", (..., " ^ layout_constr_expr expr ^ "))"
+  spf "(%s, (..., %s))" (layout_ident_decl id) (layout_constr_expr expr)
 
 let layout_theorem_kind t =
   match t with
@@ -74,6 +85,13 @@ let layout_theorem_kind t =
   | Proposition -> "Proposition"
   | Corollary -> "Corollary"
 
+let layout_assumption_kind t =
+  match t with
+  | Definitional -> "Parameter"
+  | Logical -> "Axiom"
+  | Conjectural -> "Conjecture"
+  | Context -> "Context"
+
 let layout_bullet b =
   match b with
   | Dash n -> "(Dash " ^ string_of_int n ^ ")"
@@ -83,47 +101,47 @@ let layout_bullet b =
 let layout_pure p =
   match p with
   (* | VernacFixpoint _ -> "" *)
-  | VernacStartTheoremProof (t, e) ->
+  | VernacStartTheoremProof (kind, e) ->
     let p = String.concat ", " @@ List.map layout_proof_expr e in
-    "(VernacStartTheoremProof (" ^ layout_theorem_kind t ^ ", [" ^ p ^ "]))"
+    spf "(VernacStartTheoremProof (%s, [%s]))" (layout_theorem_kind kind) p
   | VernacProof (None, None) -> "(VernacProof (None, None)) (* Proof *)"
   | VernacProof (None, Some _) -> "(VernacProof (None, Some ...)) (* Proof using *)"
   | VernacProof (Some _, None) -> "(VernacProof (Some ..., None)) (* Proof with *)"
   | VernacProof (Some _, Some _) -> "(VernacProof (Some ..., Some ...)) (* Proof with using *)"
-  | VernacExactProof p -> "(VernacExactProof " ^ layout_constr_expr p ^ ")"
+  | VernacExactProof p -> spf "(VernacExactProof %s)" @@ layout_constr_expr p
   | VernacEndProof Admitted -> "(VernacEndProof Admitted)"
   | VernacEndProof (Proved (Transparent, None)) -> "(VernacEndProof (Proved (Transparent, None))) (* Defined *)"
   | VernacEndProof (Proved (Opaque, None)) -> "(VernacEndProof (Proved (Opaque, None))) (* Qed *)"
-  | VernacEndProof (Proved (Opaque, Some l)) -> "(VernacEndProof (Proved (Opaque, Some " ^ layout_lident l ^ ")))"
-  | VernacBullet b -> "(VernacBullet " ^ layout_bullet b ^ ")"
+  | VernacEndProof (Proved (Opaque, Some l)) -> spf "(VernacEndProof (Proved (Opaque, Some %s)))" @@ layout_lident l
+  | VernacBullet b -> spf "(VernacBullet %s)" @@ layout_bullet b
   | _ -> "unknown_pure"
 
 let layout_synterp p =
   match p with
-  | VernacLoad (_, s) -> "(VernacLoad (..., \"" ^ s ^ "\"))"
+  | VernacLoad (_, s) -> spf "(VernacLoad (..., \"%s\"))" s
   | VernacReservedNotation (_, _) -> "(VernacReservedNotation ...)"
   | VernacNotation (_, _) -> "(VernacNotation ...)"
-  | VernacDeclareCustomEntry s -> "(VernacDeclareCustomEntry \"" ^ s ^ "\")"
-  | VernacBeginSection l -> "(VernacBeginSection " ^ layout_lident l ^ ")"
-  | VernacEndSegment l -> "(VernacEndSegment " ^ layout_lident l ^ ")"
-  | VernacRequire (i, _, _) ->
-    let s = match i with
-    | Some q -> "(Some " ^ layout_qualid q ^ ")"
+  | VernacDeclareCustomEntry s -> spf "(VernacDeclareCustomEntry \"%s\")" s
+  | VernacBeginSection l -> spf "(VernacBeginSection %s)" @@ layout_lident l
+  | VernacEndSegment l -> spf "(VernacEndSegment %s)" @@ layout_lident l
+  | VernacRequire (id_opt, _, _) ->
+    let id = match id_opt with
+    | Some q -> spf "(Some %s)" @@ layout_qualid q
     | None -> "None"
-    in "(VernacRequire (" ^ s ^ ", ..., ...)"
+    in spf "(VernacRequire (%s, ..., ...)" id
   | VernacImport (_, _) -> "(VernacImport ...)"
-  | VernacDeclareModule (_, l, _, _) -> "(VernacDeclareModule (..., " ^ layout_lident l ^ ", ..., ...))"
-  | VernacDefineModule (_, l, _, _, _) -> "(VernacDefineModule (..., " ^ layout_lident l ^ ", ..., ..., ...)"
-  | VernacDeclareModuleType (l, _, _, _) -> "(VernacDeclareModuleType (" ^ layout_lident l ^ ", ..., ..., ...)"
+  | VernacDeclareModule (_, l, _, _) -> spf "(VernacDeclareModule (..., %s, ..., ...))" @@ layout_lident l
+  | VernacDefineModule (_, l, _, _, _) -> spf "(VernacDefineModule (..., %s, ..., ..., ...)" @@ layout_lident l
+  | VernacDeclareModuleType (l, _, _, _) -> spf "(VernacDeclareModuleType (%s, ..., ..., ...)" @@ layout_lident l
   | VernacInclude _ -> "(VernacInclude ...)"
   | VernacDeclareMLModule ls ->
-    let s = String.concat ", " ls in "(VernacDeclareMLModule [" ^ s ^ "])"
+    spf "(VernacDeclareMLModule [%s])" @@ String.concat ", " ls
   | VernacChdir None -> "(VernacChdir None)"
-  | VernacChdir (Some s) -> "(VernacChdir (Some \"" ^ s ^ "\"))"
-  | VernacExtraDependency (id, s, _) -> "(VernacExtraDependency (" ^ layout_qualid id ^ ", \"" ^ s ^ "\", ...))"
-  | VernacSetOption (b, _, _) -> "(VernacSetOption (" ^ string_of_bool b ^ ", ..., ...))"
-  | VernacProofMode s -> "(VernacProofMode \"" ^ s ^ "\")"
-  | VernacExtend (_, _) -> "(VernacExtend ...)"
+  | VernacChdir (Some dir) -> spf "(VernacChdir (Some \"%s\"))" dir
+  | VernacExtraDependency (id, s, _) -> spf "(VernacExtraDependency (%s, \"%s\", ...))" (layout_qualid id) s
+  | VernacSetOption (b, _, _) -> spf "(VernacSetOption (%b, ..., ...))" b
+  | VernacProofMode s -> spf "(VernacProofMode \"%s\")" s
+  | VernacExtend (_, _) -> "(VernacExtend ...)" (* tactics *)
 
 let layout_expr expr =
   match expr with
